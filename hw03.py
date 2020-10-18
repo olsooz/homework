256d4e240c6d4ac8ce489adfbd8c523230a3d749
olsooz <olrosesj@hotmail.com>
2020-10-18 22:50:39 +0900

    Update hw03.py
    지니top50_3주차숙제

--- hw03.py
+++ hw03.py
@@ -1,3 +1,36 @@
+# 크롤링
+# DB인써트
 
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 
-print("이수지천재")
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta    # 'dbsparta'라는 이름의 db를 사용합니다. 'dbsparta' db가 없다면 새로 만듭니다.
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20201015', headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for tr in trs :
+
+    rank = tr.select_one('td.number').text
+    trim_rank = rank.split()[0]
+    title = tr.select_one('td.info > a.title.ellipsis').text
+    trim_title = title.strip()
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+    singer = tr.select_one('td.info > a.artist.ellipsis').text
+
+    print(trim_rank, trim_title, singer)
+
+    song = {'rank': trim_rank, 'title': trim_title, 'artist': singer}
+    db.songs.insert_one(song)
+
