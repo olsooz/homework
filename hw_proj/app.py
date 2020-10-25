48d294239db410c95a7119e204b1ae0f13998f5b
olsooz <olrosesj@hotmail.com>
2020-10-25 21:51:27 +0900

    Update app.py
    1025수정

--- hw_proj/app.py
+++ hw_proj/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
-from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client.dbhomework
